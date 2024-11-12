23b4fa6f749a62cc1f952750ba405e0f374de482
MUCHIO <n061362@hotmail.co.jp>
2024-11-12 18:45:51 +0900

    Switch .env

--- config/config.py
+++ config/config.py
@@ -1,8 +1,12 @@
 from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
+import sys
+
+def is_pytest_running():
+    return 'pytest' in sys.argv[0]
 
 class Settings(BaseSettings):
     database_url: str
-    model_config = ConfigDict(env_file = '.env')
+    model_config = ConfigDict(env_file = '.env.test' if is_pytest_running() else '.env' )
 
 settings = Settings()
