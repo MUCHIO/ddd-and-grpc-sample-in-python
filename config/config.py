04249f278ca34cd051b18b89cf97e74c6420feac
MUCHIO <n061362@hotmail.co.jp>
2024-11-13 13:50:21 +0900

    Switch database_url

--- config/config.py
+++ config/config.py
@@ -1,12 +1,27 @@
-from pydantic_settings import BaseSettings
-from pydantic import ConfigDict
-import sys
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
-def is_pytest_running():
-    return 'pytest' in sys.argv[0]
+def _is_pytest_running():
+    return os.environ.get("PYTEST_VERSION") is not None
 
 class Settings(BaseSettings):
-    database_url: str
-    model_config = ConfigDict(env_file = '.env.test' if is_pytest_running() else '.env' )
+    db_driver: str
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: str
+    db_name: str
+
+    @property
+    def modified_db_name(self) -> str:
+        if _is_pytest_running():
+            return f"{self.db_name}_test"
+        return self.db_name
+
+    @property
+    def database_url(self) -> str:
+        return f"{self.db_driver}://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.modified_db_name}"
+
+    model_config = SettingsConfigDict(env_file = '.env', env_file_encoding='utf-8')
 
 settings = Settings()
