dc4612fead488738665a54064ad189a2359f7761
MUCHIO <n061362@hotmail.co.jp>
2024-11-13 14:01:51 +0900

    Refactor

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 import os
 
 def _is_pytest_running():
@@ -10,17 +11,17 @@ class Settings(BaseSettings):
     db_password: str
     db_host: str
     db_port: str
-    db_name: str
+    loaded_db_name: str = Field(..., alias='DB_NAME')
 
     @property
-    def modified_db_name(self) -> str:
+    def db_name(self) -> str:
         if _is_pytest_running():
-            return f"{self.db_name}_test"
-        return self.db_name
+            return f"{self.loaded_db_name}_test"
+        return self.loaded_db_name
 
     @property
     def database_url(self) -> str:
-        return f"{self.db_driver}://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.modified_db_name}"
+        return f"{self.db_driver}://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
     model_config = SettingsConfigDict(env_file = '.env', env_file_encoding='utf-8')
 
