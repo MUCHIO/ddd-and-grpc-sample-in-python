46827f14a3450efc1db49be9275b1a18cfc07950
MUCHIO <n061362@hotmail.co.jp>
2024-11-28 19:06:05 +0900

    Add API ends log

--- src/presentation/grpc/interceptors/logging_interceptor.py
+++ src/presentation/grpc/interceptors/logging_interceptor.py
@@ -1,13 +1,14 @@
 from grpc import ServerInterceptor
-import logging
+from logging import getLogger, StreamHandler, DEBUG
 
 class LoggingInterceptor(ServerInterceptor):
     def intercept_service(self, continuation, handler_call_details):
         method_name = handler_call_details.method
-        print(f"Received request for method: {method_name}")
-        logging.debug(f"Received request for method: {method_name}")
+        logger = getLogger(__name__)
+        logger.info("Request for {} method starts.".format(method_name))
 
         # リクエストを処理する
         response = continuation(handler_call_details)
 
+        logger.info("Request for {} method ends.".format(method_name))
         return response
