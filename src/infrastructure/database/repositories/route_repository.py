23e069d6343ff38e7c7a92dc765652b733525494
MUCHIO <n061362@hotmail.co.jp>
2024-12-24 19:14:55 +0900

    Repository qlalchemy to v2

--- src/infrastructure/database/repositories/route_repository.py
+++ src/infrastructure/database/repositories/route_repository.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from src.infrastructure.database.schemas.route import Route
 from src.domain.entities.route import RouteModel
@@ -17,35 +18,43 @@ class RouteRepository(IRouteRepository):
         return RouteModel.model_validate(db_route)
 
     def find_route(self, route_id: int) -> Optional[RouteModel]:
-        db_route = self.session.query(Route).filter(Route.id == route_id).first()
-        if db_route:
-            return RouteModel.model_validate(db_route)
+        stmt = select(Route).where(Route.id == route_id)
+        result = self.session.execute(stmt).scalar_one_or_none()
+        if result:
+            return RouteModel.model_validate(result)
         return None
 
     def find_route_by_point(self, latitude: int, longitude: int) -> RouteModel | None:
-        db_route = self.session.query(Route).filter(Route.latitude == latitude, Route.longitude == longitude).first()
-        if db_route:
-            return RouteModel.model_validate(db_route)
+        stmt = select(Route).where(
+            Route.latitude == latitude,
+            Route.longitude == longitude
+        )
+        result = self.session.execute(stmt).scalar_one_or_none()
+        if result:
+            return RouteModel.model_validate(result)
         return None
 
     def update_route(self, route_id: int, updated_route: RouteModel) -> Optional[RouteModel]:
-        db_route = self.session.query(Route).filter(Route.id == route_id).first()
-        if db_route:
+        stmt = select(Route).where(Route.id == route_id)
+        result = self.session.execute(stmt).first()
+        if result:
             for key, value in updated_route.dict().items():
-                setattr(db_route, key, value)
+                setattr(result, key, value)
             self.session.commit()
-            self.session.refresh(db_route)
-            return RouteModel.model_validate(db_route)
+            self.session.refresh(result)
+            return RouteModel.model_validate(result)
         return None
 
     def delete_route(self, route_id: int) -> bool:
-        db_route = self.session.query(Route).filter(Route.id == route_id).first()
-        if db_route:
-            self.session.delete(db_route)
+        stmt = select(Route).where(Route.id == route_id)
+        result = self.session.execute(stmt).scalar_one_or_none()
+        if result:
+            self.session.delete(result)
             self.session.commit()
             return True
         return False
 
     def list_routes(self) -> List[RouteModel]:
-        db_routes = self.session.query(Route).all()
-        return [RouteModel.model_validate(route) for route in db_routes]
+        stmt = select(Route)
+        results = self.session.execute(stmt).scalars().all()
+        return [RouteModel.model_validate(route) for route in results]
